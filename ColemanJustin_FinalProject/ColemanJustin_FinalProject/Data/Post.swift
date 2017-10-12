fef81be180540c223226d703bb0822faf2d5ada6
Justin Coleman <Justin@Js-MacBook-Pro.local>
2017-10-12 13:10:30 -0400

    Created Post Class

---
+++ ColemanJustin_FinalProject/ColemanJustin_FinalProject/Data/Post.swift
@@ -0,0 +1,24 @@
+//
+//  Post.swift
+//  ColemanJustin_FinalProject
+//
+//  Created by Justin Coleman on 10/12/17.
+//  Copyright © 2017 Justin Coleman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Post{
+    let mCaption: String?
+    let mImage: UIImage?
+    let mLikes: Int?
+    let mComments: Int?
+    
+    init(caption: String, image: UIImage, likes: Int, comments: Int){
+        mCaption = caption
+        mImage = image
+        mLikes = likes
+        mComments = comments
+    }
+}
--- ColemanJustin_FinalProject/ColemanJustin_FinalProject/Main/Feed/FeedViewController.swift
+++ ColemanJustin_FinalProject/ColemanJustin_FinalProject/Main/Feed/FeedViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MaterialComponents
 
-class FeedViewController: UIViewController {
+class FeedViewController: MDCCollectionViewController {
     
     //MARK: - Outlets
     
